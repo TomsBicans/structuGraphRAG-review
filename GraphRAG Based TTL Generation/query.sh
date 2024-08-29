a0d987e3c510449e1280af546851ba1f535545c6
scao7 <scao7@crimson.ua.edu>
2024-08-29 15:58:52 -0500

    Add files via upload

---
+++ GraphRAG Based TTL Generation/query.sh
@@ -0,0 +1,23 @@
+python -m graphrag.query --root ./ragtest_nsduh --method global "What is the code for the variable measuring age?"
+python -m graphrag.query --root ./ragtest_nsduh --method global "How is the variable for alcohol use frequency categorized?
+"
+python -m graphrag.query --root ./ragtest_nsduh --method global "What does the code '1' represent in the variable for marijuana use in the past month?
+"
+python -m graphrag.query --root ./ragtest_nsduh --method global "How is the education level of respondents coded?
+"
+python -m graphrag.query --root ./ragtest_nsduh --method global "Which variable represents the income level of respondents?
+"
+python -m graphrag.query --root ./ragtest_nsduh --method global "What are the possible responses for the variable measuring tobacco use?
+"
+python -m graphrag.query --root ./ragtest_nsduh --method global "Is there a variable that indicates participation in mental health treatment? If so, what is its code?
+"
+python -m graphrag.query --root ./ragtest_nsduh --method global "How is the variable for ethnicity coded in the dataset?
+"
+python -m graphrag.query --root ./ragtest_nsduh --method global "What codes are used to classify the employment status of respondents?
+"
+python -m graphrag.query --root ./ragtest_nsduh --method global "Which variable captures the respondents' perception of risk associated with drug use?
+"
+
+python -m graphrag.query --root ./ragtest_nsduh --method global "What is the most commonly used drug in vehicle-related illegal activities?"
+python -m graphrag.query --root ./ragtest_nsduh --method global "What are the top two most commonly used drugs in vehicle-related illegal activities?"
+python -m graphrag.query --root ./ragtest_nsduh --method global "What is the most commonly encountered drug in early exposure cases?"
